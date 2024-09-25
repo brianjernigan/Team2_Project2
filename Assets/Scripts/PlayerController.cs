86531d7642969a5fd39f4698ae03fe72c7b1455d
Brian <brobertjernigan@gmail.com>
2024-09-24 20:34:20 -0400

    Better PlayerMovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    private float _moveSpeed = 100f;
+    private float _moveSpeed = 10f;
     private float _rotationSpeed = 720f;
 
     private Rigidbody _rb;
@@ -18,27 +18,37 @@ public class PlayerController : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        HandleMovement();
+        MovePlayer();
     }
 
-    private void HandleMovement()
+    private void MovePlayer()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+        _moveDirection.z = Input.GetAxisRaw("Vertical");
 
-        _moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+        _moveDirection = _moveDirection.normalized;
 
-        if (_moveDirection.magnitude >= 0.1f)
+        var isMoving = _moveDirection != Vector3.zero;
+
+        if (!isMoving)
         {
-            var targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg;
-
-            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationSpeed, 0.1f);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            var moveOffset = transform.forward * (_moveSpeed * Time.deltaTime);
-            _rb.MovePosition(transform.position + moveOffset);
+            _rb.velocity = Vector3.zero;
         }
+        else
+        {
+            _rb.velocity = _moveDirection * _moveSpeed;
+        }
+
+        RotatePlayer();
+    }
+
+    private void RotatePlayer()
+    {
+        if (_moveDirection == Vector3.zero) return;
+        var targetAngle = Mathf.Atan2(_moveDirection.x, _moveDirection.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationSpeed, 0.1f);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 }
